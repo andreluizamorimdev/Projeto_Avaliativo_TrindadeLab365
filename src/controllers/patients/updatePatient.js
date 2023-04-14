e71ef6eedf92ebdfac7c3ccde197db01dd4f1d79
andreluizamorimdev <andreluizamorim.dev@gmail.com>
2023-04-14 04:00:50 -0300

    Fixed request name to full_name when receiving update data

--- src/controllers/patients/updatePatient.js
+++ src/controllers/patients/updatePatient.js
@@ -10,14 +10,14 @@ async function updatePatient(request, response) {
             return response.status(404).json({ message: 'Paciente não encontrado.' });
         }
 
-        patientInDatabase.full_name = request.body.name || patientInDatabase.full_name;
+        patientInDatabase.full_name = request.body.full_name || patientInDatabase.full_name;
         patientInDatabase.gender = request.body.gender || patientInDatabase.gender;
         patientInDatabase.birth_date = request.body.birth_date || patientInDatabase.birth_date;
         patientInDatabase.cpf = request.body.cpf || patientInDatabase.cpf;
         patientInDatabase.phone_number = request.body.phone_number || patientInDatabase.phone_number;
         patientInDatabase.emergency_contact = request.body.emergency_contact || patientInDatabase.emergency_contact;
         patientInDatabase.allergies_list = request.body.allergies_list || patientInDatabase.allergies_list;
-        patientInDatabase.health_insurance_plan = request.body.health_insurance_plan || patientInDatabase.health;
+        patientInDatabase.health_insurance_plan = request.body.health_insurance_plan || patientInDatabase.health_insurance_plan;
 
         await patientInDatabase.save();
 
@@ -25,7 +25,7 @@ async function updatePatient(request, response) {
 
     } catch (error) {
         console.log(error);
-        response.status(500).json({ message: 'Não foi possível processar a solicitação' })
+        response.status(500).json({ message: 'Não foi possível processar a solicitação' });
     }
 }
 
